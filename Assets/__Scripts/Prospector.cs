8d29b28b4d88761e009b47778e2c2c2c9e2f180a
Luthere15 <42876725+Luthere15@users.noreply.github.com>
2018-10-31 17:23:57 -0400

    668

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -10,11 +10,13 @@ public class Prospector : MonoBehaviour {
 	static public Prospector 	S;
 
 	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
+	public TextAsset deckXML;
+    public TextAsset layoutXML;
 
 
 	[Header("Set Dynamically")]
-	public Deck					deck;
+	public Deck	  deck;
+    public Layout layout;
 
 	void Awake(){
 		S = this;
@@ -23,6 +25,17 @@ public class Prospector : MonoBehaviour {
 	void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
+        Deck.Shuffle(ref deck.cards);
+
+        Card c;
+        for(int cNum=0; cNum<deck.cards.Count;cNum++)
+        {
+            c = deck.cards[cNum];
+            c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 3, 0);
+        }
+
+        layout = GetComponent<Layout>();
+        layout.ReadLayout(layoutXML.text);
 	}
 
 }
